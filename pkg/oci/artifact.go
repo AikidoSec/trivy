e6337c5c09e6ca60dbeaab4abfb8ed15f17025e3
Tudor TIMCU <tudor@aikido.dev>
2026-02-13 20:39:24 +0200

    refactor(oci): simplify error handling in artifact download process by removing unnecessary repository retry logic

--- pkg/oci/artifact.go
+++ pkg/oci/artifact.go
@@ -236,9 +236,6 @@ func (a Artifacts) Download(ctx context.Context, dst string, opt DownloadOption)
 			return nil
 		}
 
-		if !shouldTryOtherRepo(err) {
-			return xerrors.Errorf("failed to download artifact from %s: %w", art.repository, err)
-		}
 		log.ErrorContext(ctx, "Failed to download artifact", log.String("repo", art.repository), log.Err(err))
 		if i < len(a)-1 {
 			log.InfoContext(ctx, "Trying to download artifact from other repository...")
@@ -252,7 +249,7 @@ func (a Artifacts) Download(ctx context.Context, dst string, opt DownloadOption)
 func shouldTryOtherRepo(err error) bool {
 	var terr *transport.Error
 	if !errors.As(err, &terr) {
-		return true // If we fail to fetch the Aikido Container Registry -> always try the next repository
+		return false
 	}
 
 	for _, diagnostic := range terr.Errors {
@@ -278,5 +275,5 @@ func shouldTryOtherRepo(err error) bool {
 		}
 	}
 
-	return true // If we fail to fetch the Aikido Container Registry -> always try the next repository
+	return false
 }
