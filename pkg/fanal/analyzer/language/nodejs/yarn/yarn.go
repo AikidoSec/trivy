669772a723bbc3462892d0ab93dd9ac81800ab90
Tudor TIMCU <tudor@aikido.dev>
2026-03-04 13:21:17 +0200

    Fix logic for determining development dependencies in yarn analyzer

--- pkg/fanal/analyzer/language/nodejs/yarn/yarn.go
+++ pkg/fanal/analyzer/language/nodejs/yarn/yarn.go
@@ -310,7 +310,7 @@ func (a yarnAnalyzer) walkDependencies(parent *types.Package, pkgs map[string]ty
 		// If the package is already marked as a production dependency, skip overwriting it.
 		// Since the dev field is boolean, it cannot determine if the package is already processed,
 		// so we need to check the relationship field.
-		if pkg.Relationship == types.RelationshipUnknown || pkg.Dev {
+		if pkg.Relationship == types.RelationshipUnknown && pkg.Dev != true {
 			pkg.Dev = dev
 		}
 
@@ -342,7 +342,7 @@ func (a yarnAnalyzer) walkIndirectDependencies(pkg types.Package, pkgs map[strin
 			continue
 		}
 
-		if dep.Relationship == types.RelationshipUnknown || dep.Dev {
+		if dep.Relationship == types.RelationshipUnknown && dep.Dev != true {
 			dep.Dev = pkg.Dev
 		}
 		dep.Indirect = true
